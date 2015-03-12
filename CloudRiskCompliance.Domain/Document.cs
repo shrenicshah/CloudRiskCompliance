9e13ca9b21c565c69fc5d3ed075eddacf93fecbb
shrenicshah <shrenic.shah@gmail.com>
2015-03-12 21:38:18 +1100

    Architechture initial commit

---
+++ CloudRiskCompliance.Domain/Document.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CloudRiskCompliance.DomainClasses
+{
+    public class Document
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
